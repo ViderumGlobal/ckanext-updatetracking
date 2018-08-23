994c0c447387fec9285f59ee813e99234f239347
Aleksandar Jovanov <aleksandar.jovanov@keitaro.com>
2018-08-23 12:05:31 +0200

    Fix update command

--- ckanext/updatetracking/commands.py
+++ ckanext/updatetracking/commands.py
@@ -4,13 +4,14 @@ import ckanapi
 import ConfigParser
 import time
 from ckan.lib.cli import CkanCommand
+from ckan.common import config as ckan_config
 
 
 class Update(CkanCommand):
 
     ''' Updates a resource with a daily tracking report
         Usage:
-          update 
+          update
             - Updates the resource with the new tracking report
     '''
 
@@ -18,45 +19,47 @@ class Update(CkanCommand):
     usage = __doc__
 
     def command(self):
-        self._load_config()
-	PATH = os.path.dirname(os.path.abspath(__file__))
+		self._load_config()
 
-	config = ConfigParser.RawConfigParser()
-	config.read(PATH + '/updateconfig.cfg')
-	FILENAME = config.get('DATASET','filename')
-	DATASET = config.get('DATASET','dataset')
-	SITE = config.get('CKAN','site')
-	API = config.get('CKAN','api')
-	
-	try:
-	    ckan = ckanapi.RemoteCKAN(SITE, apikey=API)
-	except:
-	    print("Ckan instance could not be initialized")
-	    sys.exit()
-	try:
-	    dataset = ckan.action.package_show(id=DATASET)
-	except:
-	    print("Dataset: " + DATASET + " could not be found")
-	    sys.exit()
-	#try:
-	#    file = open(FILENAME,'rb')
-	#except:
-	#    print("File was not found")
-	    
-	
-	#Call the ckan tracking command
-	os.system("cd /usr/lib/ckan/default/src/ckan && /usr/lib/ckan/default/bin/python /usr/lib/ckan/default/bin/paster tracking export " + FILENAME + " --config=/etc/ckan/default/production.ini")
-	
-	try:
-	    file = open(FILENAME,'rb')
-	except:
-	    print("File was not generated")
-	    sys.exit()
-	for i in dataset["resources"]:
-	    url = i["url"]
-	    ckan.action.resource_update(id=i["id"],package_id=i["package_id"],url=i["url"],upload=file,name=file.name,format=i["format"])
-	    #Trigger the datapusher hook the dirty way
-	    ckan.action.resource_update(id=i["id"],package_id=i["package_id"],name=file.name,format=i["format"],url=i["url"]+"&")
-	    ckan.action.resource_update(id=i["id"],package_id=i["package_id"],name=file.name,format=i["format"],url=url)
-	
-	sys.exit()
+		PATH = os.path.dirname(os.path.abspath(__file__))
+
+		config = ConfigParser.RawConfigParser()
+		config.read(PATH + '/updateconfig.cfg')
+		FILENAME = config.get('DATASET','filename')
+		DATASET = config.get('DATASET','dataset')
+		SITE = config.get('CKAN','site')
+		API = config.get('CKAN','api')
+
+		try:
+			ckan = ckanapi.RemoteCKAN(SITE, apikey=API)
+		except:
+			print("Ckan instance could not be initialized")
+			sys.exit()
+		try:
+			dataset = ckan.action.package_show(id=DATASET)
+		except:
+			print("Dataset: " + DATASET + " could not be found")
+			sys.exit()
+		try:
+		   file = open(FILENAME,'rb')
+		except:
+		   print("File was not found")
+
+		config_file_location = ckan_config['__file__']
+
+		#Call the ckan tracking command
+		os.system("paster --plugin=ckan tracking export " + FILENAME + " --config=" + config_file_location)
+
+		try:
+			file = open(FILENAME,'rb')
+		except:
+			print("File was not generated")
+			sys.exit()
+		for i in dataset["resources"]:
+			url = i["url"]
+			ckan.action.resource_update(id=i["id"],package_id=i["package_id"],url=i["url"],upload=file,name=file.name,format=i["format"])
+			#Trigger the datapusher hook the dirty way
+			ckan.action.resource_update(id=i["id"],package_id=i["package_id"],name=file.name,format=i["format"],url=i["url"]+"&")
+			ckan.action.resource_update(id=i["id"],package_id=i["package_id"],name=file.name,format=i["format"],url=url)
+
+		sys.exit()
